83140d5255659746dd6b223aed15d8bfe834a898
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-06-29 19:13:39 -0300

    Make RenoteAuthentication implements Authentication

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -6,7 +6,7 @@ import '../../domain/entity/entity.dart';
 import '../../data/model/model.dart';
 import '../http/http.dart';
 
-class RemoteAuthentication {
+class RemoteAuthentication implements Authentication {
   final HttpClient httpClient;
   final String url;
 
