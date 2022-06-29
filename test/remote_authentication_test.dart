beede39fdcd92674e786d208198935e40ed7509b
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-06-29 15:27:21 -0300

    Ensure RemoteAuthentication throws UnexpectedError on 400

--- test/remote_authentication_test.dart
+++ test/remote_authentication_test.dart
@@ -3,6 +3,7 @@ import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 
 import 'package:sales_micros_services/domain/usecases/usecases.dart';
+import 'package:sales_micros_services/domain/helpers/helpers.dart';
 import 'package:sales_micros_services/data/usecases/usecases.dart';
 import 'package:sales_micros_services/data/http/http.dart';
 
@@ -30,4 +31,15 @@ void main() {
         body: {'email': params.email, 'password': params.secret}
       ));
   });
+
+  test('Shoul throw Unexpected if HttpClinete returns 400', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+      .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
+
 }
