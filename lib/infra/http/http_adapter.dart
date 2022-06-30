1f70056a6606fd638af60395f5b0abbc666cbfc6
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-06-30 15:04:34 -0300

    Move duplicated code to a private method

---
+++ lib/infra/http/http_adapter.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart';
+import 'package:meta/meta.dart';
+import 'dart:convert';
+
+import '../../data/http/http.dart';
+
+class HttpAdapter implements HttpClient {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<Map> request({@required Uri uri, @required String method, Map body}) async {
+    final headers = {
+      'content-type': 'application/json',
+      'accept': 'application/json'
+    };
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    final response = await client.post(uri, headers: headers, body: jsonBody);
+    return _handleResponse(response);
+  }
+
+  Map _handleResponse(Response response) {
+    if(response.statusCode == 200) {
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    } else {
+      return null;
+    }
+  }
+}
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,32 +1,9 @@
-import 'dart:convert';
-
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
-import 'package:meta/meta.dart';
 import 'package:http/http.dart';
 import 'package:test/test.dart';
 
-import 'package:sales_micros_services/data/http/http.dart';
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request({@required Uri uri, @required String method, Map body}) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json'
-    };
-    final jsonBody = body != null ? jsonEncode(body) : null;
-    final response = await client.post(uri, headers: headers, body: jsonBody);
-    if(response.statusCode == 200) {
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    } else {
-      return null;
-    }
-  }
-}
+import 'package:sales_micros_services/infra/http/http.dart';
 
 class ClientSpy extends Mock implements Client {}
 
