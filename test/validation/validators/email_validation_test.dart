78984210ac20fa34a64f7c3fd6c8d7757b107a92
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-07-05 09:08:06 -0300

    Ensure EmailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -2,13 +2,14 @@ import 'package:delivery_micros_services/validators/protocols/field_validation.d
 import 'package:test/test.dart';
 
 class EmailValidation implements FieldValidation {
-
   final String field;
 
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final regex = RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido';
   }
 
 }
@@ -30,6 +31,10 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('rrs.sistema@gmail.com'), null);
+  });  
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('rrs.sistema'), 'Campo inválido');
   });  
 
 }
