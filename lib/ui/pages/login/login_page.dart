e6e965d15673f7fc134d4b2bc8a9eed89c29e123
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-07-05 15:50:53 -0300

    Hide keyboard os tap

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -16,6 +16,14 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+
+  void _hideKayboard() {
+    final currectFocus = FocusScope.of(context);
+    if(!currectFocus.hasPrimaryFocus){
+      currectFocus.unfocus();
+    }
+  }
+
   @override
   void dispose() {
     super.dispose();
@@ -44,86 +52,89 @@ class _LoginPageState extends State<LoginPage> {
           }
         });
 
-        return SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: _headerHeight,
-                child: HeaderWidget(
-                    _headerHeight,
-                    true,
-                    Icons
-                        .shopping_cart_outlined), //vamos criar um widget de cabeçalho comum
-              ),
-              SafeArea(
-                child: Container(
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  margin: EdgeInsets.fromLTRB(
-                      20, 10, 20, 10), // Este será o formulário de login
-                  child: Column(
-                    children: [
-                      Text(
-                        'Delivery Micros Services',
-                        style: TextStyle(
-                            fontSize: 25, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        'Fazer login na sua conta',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      SizedBox(height: 30.0),
-                      Provider(
-                        create: (_) => widget.presenter,
-                        child: Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              Container(
-                                child: EmailInput(),
-                                decoration:
-                                    ThemeHelper().inputBoxDecorationShaddow(),
-                              ),
-                              SizedBox(height: 30.0),
-                              Container(
-                                child: PasswordInput(),
-                                decoration:
-                                    ThemeHelper().inputBoxDecorationShaddow(),
-                              ),
-                              SizedBox(height: 15.0),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
-                                alignment: Alignment.topRight,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    //Navigator.push( context, MaterialPageRoute( builder: (context) => ForgotPasswordPage()), );
-                                  },
-                                  child: Text(
-                                    "Esqueceu sua senha?",
-                                    style: TextStyle(
-                                      color: Colors.grey,
+        return GestureDetector(
+          //onTap: _hideKayboard,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: _headerHeight,
+                  child: HeaderWidget(
+                      _headerHeight,
+                      true,
+                      Icons
+                          .shopping_cart_outlined), //vamos criar um widget de cabeçalho comum
+                ),
+                SafeArea(
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                    margin: EdgeInsets.fromLTRB(
+                        20, 10, 20, 10), // Este será o formulário de login
+                    child: Column(
+                      children: [
+                        Text(
+                          'Delivery Micros Services',
+                          style: TextStyle(
+                              fontSize: 25, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'Fazer login na sua conta',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        SizedBox(height: 30.0),
+                        Provider(
+                          create: (_) => widget.presenter,
+                          child: Form(
+                            key: _formKey,
+                            child: Column(
+                              children: [
+                                Container(
+                                  child: EmailInput(),
+                                  decoration:
+                                      ThemeHelper().inputBoxDecorationShaddow(),
+                                ),
+                                SizedBox(height: 30.0),
+                                Container(
+                                  child: PasswordInput(),
+                                  decoration:
+                                      ThemeHelper().inputBoxDecorationShaddow(),
+                                ),
+                                SizedBox(height: 15.0),
+                                Container(
+                                  margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                  alignment: Alignment.topRight,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      //Navigator.push( context, MaterialPageRoute( builder: (context) => ForgotPasswordPage()), );
+                                    },
+                                    child: Text(
+                                      "Esqueceu sua senha?",
+                                      style: TextStyle(
+                                        color: Colors.grey,
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              Container(
-                                decoration:
-                                    ThemeHelper().buttonBoxDecoration(context),
-                                child: LoginButton(),
-                              ),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
-                                //child: Text('Don\'t have an account? Create'),
-                                child: CreateLoginButton(),
-                              ),
-                            ],
+                                Container(
+                                  decoration:
+                                      ThemeHelper().buttonBoxDecoration(context),
+                                  child: LoginButton(),
+                                ),
+                                Container(
+                                  margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                  //child: Text('Don\'t have an account? Create'),
+                                  child: CreateLoginButton(),
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         );
       }),
