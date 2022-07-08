c3371fceabe6f4df496d596e1cda24d7c526e155
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-07-08 08:32:46 -0300

    Ensure LocalStorageAdapter throws if save secure throws

--- test/infra/http/cache/local_storage_adapter_test.dart
+++ test/infra/http/cache/local_storage_adapter_test.dart
@@ -6,19 +6,18 @@ import 'package:meta/meta.dart';
 
 import 'package:delivery_micros_services/data/cache/cache.dart';
 
-class LocalStorageAdapter implements SaveSecureCacheStorage{
+class LocalStorageAdapter implements SaveSecureCacheStorage {
   final FlutterSecureStorage secureStorage;
 
   LocalStorageAdapter({@required this.secureStorage});
 
-  Future<void> saveSecure({@required String key, @required String value}) async{
+  Future<void> saveSecure(
+      {@required String key, @required String value}) async {
     await secureStorage.write(key: key, value: value);
   }
 }
 
-class FlutterSecureStorageSpy extends Mock implements  FlutterSecureStorage{
-
-}
+class FlutterSecureStorageSpy extends Mock implements FlutterSecureStorage {}
 
 void main() {
   LocalStorageAdapter sut;
@@ -33,9 +32,22 @@ void main() {
     value = faker.guid.guid();
   });
 
-  test('Should call save secure with correct values', () async{
+  void mockSaveSecureError() {
+    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
+
+  test('Should call save secure with correct values', () async {
     await sut.saveSecure(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
+  });
+
+  test('Should throw if save serure throws', () {
+    mockSaveSecureError();
+
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(TypeMatcher<Exception>()));
   });
 }
