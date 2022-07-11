e66dca7a9f15a6fd3fed1c9be811e965d3876c65
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-07-11 10:43:56 -0300

    Ensure LocalStorageAdapter calls fetchSecure with correct value

--- lib/infra/cache/local_storage_adapter.dart
+++ lib/infra/cache/local_storage_adapter.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:meta/meta.dart';
 
-import '../../data/cache/save_secure_cache_storage.dart';
+import '../../data/cache/cache.dart';
 
 class LocalStorageAdapter implements SaveSecureCacheStorage {
   final FlutterSecureStorage secureStorage;
@@ -12,4 +12,10 @@ class LocalStorageAdapter implements SaveSecureCacheStorage {
       {@required String key, @required String value}) async {
     await secureStorage.write(key: key, value: value);
   }
+
+ Future<void> fetchSecure(String key) async {
+    await secureStorage.read(key: key);
+  }
+ 
+
 }
