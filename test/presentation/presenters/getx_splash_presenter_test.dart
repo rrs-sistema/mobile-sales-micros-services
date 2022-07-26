8393e0426fd18578d3d0c3377091f387646d8bf3
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-07-26 10:48:32 -0300

    Ensure SplashPresenter goes to login if accessToken is null

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -41,6 +41,14 @@ void main() {
   });
 
   test('Should go to login page on null result', () async {
+    mockLoadCurrentAccount(account: AccountEntity(accessToken: null));
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
+
+    await sut.checkAccount(durationInSeconds: 0);
+  });
+
+  test('Should go to login page on null token', () async {
     mockLoadCurrentAccount(account: null);
 
     sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
