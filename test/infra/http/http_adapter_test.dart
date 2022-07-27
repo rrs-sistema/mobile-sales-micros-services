ff8a7895f5e86b45513cf06f66dcdf7b40e2974c
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-07-27 12:43:03 -0300

    Ensure HttpAdapter throws ServerError on 500

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -247,6 +247,14 @@ void main() {
       expect(future, throwsA(HttpError.notFound));
     });
 
+    test('Should return ServerError if get returns 500', () async {
+      mockResponse(500);
+
+      final future = sut.request(uri: uri, method: 'get');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
   });
 
 }
