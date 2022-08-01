92ab635957381fcd710766e297f8434208b0420e
Rivaldo Roberto <rrs.sistema@gmail.com>
2022-08-01 11:52:53 -0300

    Make LocalLoadProducts implements LoadProducts

--- lib/data/usecases/load_products/local_load_products.dart
+++ lib/data/usecases/load_products/local_load_products.dart
@@ -1,11 +1,12 @@
 import 'package:meta/meta.dart';
 
 import './../../../domain/helpers/domain_error.dart';
+import './../../../domain/usecases/usecases.dart';
 import './../../../domain/entities/entities.dart';
 import './../../cache/cache.dart';
 import './../../model/model.dart';
 
-class LocalLoadProducts {
+class LocalLoadProducts implements LoadProducts {
   final FetchCacheStorage fetchCacheStorage;
 
   LocalLoadProducts({@required this.fetchCacheStorage});
